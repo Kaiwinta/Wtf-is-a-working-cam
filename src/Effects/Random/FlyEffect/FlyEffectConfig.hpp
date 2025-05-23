d9b0cbcdf7334813648eb6882d069ab4ad4fc80f
Kaiwinta <titouan.pradoura@epitech.eu>
2025-05-23 10:40:49 +0200

    [fix]: Remove useless logs

--- src/Effects/Random/FlyEffect/FlyEffectConfig.hpp
+++ src/Effects/Random/FlyEffect/FlyEffectConfig.hpp
@@ -17,12 +17,9 @@ namespace camshit::effects::random::flyEffect {
         FlyEffectConfig(std::string params) {
             size_t pos = params.find("frequency:\"");
             if (pos != std::string::npos) {
-                std::cout << "Found frequency parameter in params: " << params << std::endl;
                 size_t endPos = params.find("\"", pos + 11);
-                std::cout << "Sub string from pos + 11: " << params.substr(pos + 11) << std::endl;
                 if (endPos != std::string::npos) {
                     std::string frequencyStr = params.substr(pos + 11, endPos - pos - 11);
-                    std::cout << "Frequency: " << frequencyStr << std::endl;
                     frequency = std::stoi(frequencyStr);
                 }
             }
